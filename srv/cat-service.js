7a0fa88ac2f0d184c02ef670a4167d36ed937b2e
Yauheni Kalodka <yauheni.kalodka@leverx.com>
2023-09-20 08:58:21 +0000

    feat: add challenge for week 2

---
+++ srv/cat-service.js
@@ -0,0 +1,35 @@
+const cds = require('@sap/cds') 
+
+module.exports = class DevChallengeService extends cds.ApplicationService { 
+    init() { 
+        return super.init() 
+    } 
+ 
+    async assignQuestionsToTest(Tests, keys, questionsCount) {
+		if (questionsCount < 1) {
+			return "At least the questionsCount should be 1.";
+		}
+
+		const { Questions } = cds.entities;
+		const questions = await SELECT.from(Questions).where({ test_ID: null });
+		if (!questions.length) {
+			return "No questions without an test associated.";
+		}
+
+		let count = 0;
+		for (let index = 0; index < questions.length; index++) {
+			await UPDATE(Questions)
+				.set({ test_ID: keys.ID })
+				.where({ ID: questions[index].ID });
+
+                count++;
+			if (count == questionsCount) {
+				break;
+			}
+		}
+
+		return `${count} question(s) successfully added to the test.`;
+	}
+
+    
+} 
